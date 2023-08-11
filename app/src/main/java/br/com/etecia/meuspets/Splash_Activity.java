db7b0f0a97bd106f161f8df8b2131b4ae97a5d8c
LeonardoCaa <lleonardocamargost@gmail.com>
2023-08-10 22:08:43 -0300

    Atualização

--- app/src/main/java/br/com/etecia/meuspets/Splash_Activity.java
+++ app/src/main/java/br/com/etecia/meuspets/Splash_Activity.java
@@ -9,6 +9,6 @@ public class Splash_Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.splash_layout);
     }
 }
